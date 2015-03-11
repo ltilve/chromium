9765fc9dea3ab273fcada2d8a30b97b5d09f16b1
Kevin Fowler <kfowler@connectifier.com>
2015-03-11 14:37:23 -0700

    Delete trailing whitespace.

--- chrome/browser/ui/cocoa/view_id_util_browsertest.mm
+++ chrome/browser/ui/cocoa/view_id_util_browsertest.mm
@@ -77,7 +77,7 @@ class ViewIDTest : public InProcessBrowserTest {
 
     for (int i = VIEW_ID_TOOLBAR; i < VIEW_ID_PREDEFINED_COUNT; ++i) {
       // Mac implementation does not support following ids yet.
-      if (i == VIEW_ID_STAR_BUTTON || 
+      if (i == VIEW_ID_STAR_BUTTON ||
           i == VIEW_ID_BROWSER_ACTION ||
           i == VIEW_ID_SIDE_BAR_SPLIT ||
           i == VIEW_ID_FEEDBACK_BUTTON ||
