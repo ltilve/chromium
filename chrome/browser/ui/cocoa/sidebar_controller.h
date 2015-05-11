3d9f1cb64248ba3d224984ef0cb77a73e5371900
Gwang Yoon Hwang <yoon@igalia.com>
2015-05-11 18:08:51 +0900

    Fix convention violation for CL
    Do not use #pragma once in header files.
    See http://www.chromium.org/developers/coding-style#TOC-File-headers
      chrome/browser/ui/cocoa/sidebar_controller.h

--- chrome/browser/ui/cocoa/sidebar_controller.h
+++ chrome/browser/ui/cocoa/sidebar_controller.h
@@ -4,7 +4,6 @@
 
 #ifndef CHROME_BROWSER_UI_COCOA_SIDEBAR_CONTROLLER_H_
 #define CHROME_BROWSER_UI_COCOA_SIDEBAR_CONTROLLER_H_
-#pragma once
 
 #import "base/mac/cocoa_protocols.h"
 #include "base/mac/scoped_nsobject.h"
