77c5c2185c8da7903043351c7f3b3cce511acd2f
Lorenzo Tilve <ltilve@igalia.com>
2015-06-17 10:28:19 +0200

    ps5: Remove unused LoadURL()

--- extensions/browser/extension_host.h
+++ extensions/browser/extension_host.h
@@ -142,9 +142,6 @@ class ExtensionHost : public DeferredStartRenderHost,
   // Navigates to the initial page.
   virtual void LoadInitialURL();
 
-  // Navigates to the page.
-  virtual void LoadURL(const GURL& url);
-
   // Returns true if we're hosting a background page.
   virtual bool IsBackgroundPage() const;
 
