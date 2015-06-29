f7f9b37478919b6349da2417abc1ac8956d45641
Lorenzo Tilve <ltilve@igalia.com>
2015-06-29 11:41:11 +0200

    ps7: Move SidebarSwitched code to UI patch set

--- chrome/browser/extensions/sidebar_manager_observer.h
+++ chrome/browser/extensions/sidebar_manager_observer.h
@@ -21,11 +21,6 @@ class SidebarManagerObserver {
   virtual void OnSidebarHidden(content::WebContents* tab,
                                const std::string& content_id) {}
 
-  // Called when sidebar is switched from one extension to another.
-  virtual void OnSidebarSwitched(content::WebContents* old_tab,
-                                 const std::string& old_content_id,
-                                 content::WebContents* new_tab,
-                                 const std::string& new_content_id) {}
 };
 
 #endif  // CHROME_BROWSER_EXTENSIONS_SIDEBAR_MANAGER_OBSERVER_H_
